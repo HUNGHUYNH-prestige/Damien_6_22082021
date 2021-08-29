acff43af92be0ec049738797306be70de25d5d49
Hung <hunghuynh22@gmail.com>
2021-08-29 02:42:15 +0200

    http becomes https

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-// import http package from Node :
-const http = require('http');
+// import http or https package from Node :
+const https = require('https');
 
 // import the app from app.js :
 const app = require('./app');
