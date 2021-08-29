82c72d1185c8294ca704b3d4ea5d9bb746242766
Hung <hunghuynh22@gmail.com>
2021-08-29 19:30:41 +0200

    test regex input

--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
+const regexInput = /^[a-zA-Z0-9]*$/;
 
 const sauceSchema = mongoose.Schema({
     userId :        {type:String, require:false},
 
-    name :          {type:String, require:true, validate:[/^[a-zA-Z0-9 '"!?.-]+$/, 'Input name not accepted']},
-    manufacturer :  {type:String, require:true, validate:[/^[a-zA-Z0-9 '"!?.-]+$/, 'Input manufacturer not accepted']},
-    description :   {type:String, require:true, validate:[/^[a-zA-Z0-9 '"!?.-]+$/, 'Input description not accepted']},
-    mainPepper :    {type:String, require:true, validate:[/^[a-zA-Z0-9 '"!?.-]+$/, 'Input main pepper not accepted']},
+    name :          {type:String, require:true, validate:[regexInput, 'Input name not accepted'], match : [regexInput, 'Input not correct']},
+    manufacturer :  {type:String, require:true, validate:[regexInput, 'Input manufacturer not accepted'], match : [regexInput, 'Input not correct']},
+    description :   {type:String, require:true, validate:[regexInput, 'Input description not accepted'], match : [regexInput, 'Input not correct']},
+    mainPepper :    {type:String, require:true, validate:[regexInput, 'Input main pepper not accepted'], match : [regexInput, 'Input not correct']},
     imageUrl :      {type:String, require:false},
     heat :          {type:Number, require:true},
     likes :         {type:Number, require:false},
@@ -17,7 +18,8 @@ const sauceSchema = mongoose.Schema({
 
 module.exports = mongoose.model('Sauce', sauceSchema);
 
-/* solution 1
+/* 
+- - - solution 1
 
 /^[A-Za-z]+$/
 description :
@@ -27,9 +29,16 @@ all from A to Z
 all from a to z
 + meaning : 1 or more repetition
 
-*/
 
-/* solution 2
+- - - solution 2
+^[a-zA-Z0-9]*$
+description :
+^ is the beginning of the line anchor
+$ is the end of the line anchor
+[...] is a character class definition
+* is "zero-or-more" repetition
+
+- - - solution 3
 
 /^[a-zA-Z0-9 "!?.-]+$/
 description :
@@ -38,5 +47,21 @@ allows from A to Z
 allows from 0 to 9
 allows symbols like ? and !
 allows space
+
+
+
+- - - solution 4
+pour du texte en français
+/^[a-zA-ZÀ-ÿ-. ]*$/
+description :
+^         Start of the string
+[ ... ]*  Zero or more of the following:
+  a-z     lowercase alphabets
+  A-Z     Uppercase alphabets
+  À-ÿ     Accepts lowercase and uppercase characters including letters with an umlaut
+  -         dashes
+  .         periods
+            spaces
+$         End of the string
 
 */
