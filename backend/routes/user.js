af75b0ed528820632d44698c0d0b3252dde8dc37
Hung <hunghuynh22@gmail.com>
2021-08-29 04:39:44 +0200

    login max 3 with express-rate-limit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,8 +2,17 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 const passwordValidator = require('../middleware/passwordValidator');
+const rateLimit = require('express-rate-limit');
+
+const loginLimit = rateLimit({
+    windowMs : 60 * 3 * 1000,
+    max : 3
+});
+
+//const troisMinutes = 3 * 60 * 1000;
+//console.log(troisMinutes + ' millisecondes = 3 minutes');
 
 router.post('/signup', passwordValidator, userCtrl.signup);
-router.post('/login' ,                    userCtrl.login);
+router.post('/login' , loginLimit       , userCtrl.login);
 
 module.exports = router;
